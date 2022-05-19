f74f72a0ed4b135ccd4204b8049aac61a81b9cf5
Artem <maiorov.artem@gmail.com>
2022-05-19 20:59:00 +0200

    test workflow

--- tests/reqResp.spec.ts
+++ tests/reqResp.spec.ts
@@ -1,21 +1,17 @@
-// import { BMB } from "browser-message-broker";
-// import { expect } from "@open-wc/testing";
+import { BMB } from "browser-message-broker";
+import { expect } from "@open-wc/testing";
 
-// describe("Browser Message Broker subscriber", () => {
-//   let received = false;
-//   let receivedPayload = "";
+describe("Browser Message Broker subscriber", () => {
+  let received = false;
 
-//   //   BMB.Subscribe<TestMsg>(TestMsg.name, (x) => {
-//   //     received = true;
-//   //     receivedPayload = x.payload;
-//   //   });
+  //   BMB.Subscribe<TestMsg>(TestMsg.name, (x) => {
+  //     received = true;
+  //     receivedPayload = x.payload;
+  //   });
 
-//   //   before(async () => {
-//   //     await BMB.Publish(TestMsg.name, new TestMsg());
-//   //   });
+  //   before(async () => {
+  //     await BMB.Publish(TestMsg.name, new TestMsg());
+  //   });
 
-//   it("should receive published message", async () => {
-//     expect(received).to.be.true;
-//   });
-// });
-export {};
+  //it("should receive response from another ", async () => {});
+});
